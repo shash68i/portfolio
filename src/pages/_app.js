4c28072e4edcd6b4e19783dded567ccc03debda9
shash68i <theshashank6870@gmail.com>
2023-03-28 00:33:04 +0530

    Updated the Theming for SSR

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,32 +1,14 @@
-import { useState, createContext } from "react";
-import { ThemeProvider } from "styled-components";
+import ThemeProvider from "../providers/ThemeProvider";
 import Layout from "@components/layout";
 
 import "../styles/globals.css";
 
-const ThemeContext = createContext();
-
 export default function App({ Component, pageProps }) {
-  const [theme, setTheme] = useState(() => {
-    const storedTheme = localStorage.getItem("portfolio.theme");
-    if (storedTheme !== null) return storedTheme;
-    return window.matchMedia("(prefers-color-scheme: light)").matches
-      ? "light"
-      : "dark";
-  });
-
-  const changeTheme = (theme) => {
-    setTheme(theme);
-    localStorage.setItem("portfolio.theme", theme);
-  };
-
   return (
-    <ThemeContext.Provider value={changeTheme}>
-      <ThemeProvider theme={theme}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
-    </ThemeContext.Provider>
+    <ThemeProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
---
+++ src/providers/ThemeProvider/index.js
@@ -0,0 +1,40 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { ThemeProvider as SCThemeProvider } from "styled-components";
+import themes from "../../themes";
+
+export const ThemeContext = createContext({});
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("portfolio.theme");
+
+    if (storedTheme) {
+      setTheme(storedTheme);
+    } else {
+      const isBrowserThemeLight = window.matchMedia(
+        "(prefers-color-scheme: light)"
+      ).matches;
+      setTheme(isBrowserThemeLight ? "light" : "dark");
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("portfolio.theme", newTheme);
+  };
+
+  return (
+    <ThemeContext.Provider value={toggleTheme}>
+      <SCThemeProvider theme={theme === "light" ? themes.light : themes.dark}>
+        {children}
+      </SCThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
+
+export default ThemeProvider;
