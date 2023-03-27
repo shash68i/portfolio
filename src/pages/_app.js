db9da99906fa329da278af2381df7cd7b2087b78
shash68i <theshashank6870@gmail.com>
2023-03-27 22:54:39 +0530

    Theme setup done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,28 @@
+import { useState, createContext } from "react";
+import { ThemeProvider } from "styled-components";
 import "../styles/globals.css";
 
+const ThemeContext = createContext();
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [theme, setTheme] = useState(() => {
+    const storedTheme = localStorage.getItem("portfolio.theme");
+    if (storedTheme !== null) return storedTheme;
+    return window.matchMedia("(prefers-color-scheme: light)").matches
+      ? "light"
+      : "dark";
+  });
+
+  const changeTheme = (theme) => {
+    setTheme(theme);
+    localStorage.setItem("portfolio.theme", theme);
+  };
+
+  return (
+    <ThemeContext.Provider value={changeTheme}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
 }
